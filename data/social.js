7598fe87c6f0f8db27f6fb7112329c3636ae34fb
Joey Schrader <jschrader@overstock.com>
2020-04-22 19:50:14 -0600

    partial and data ready for social

---
+++ data/social.js
@@ -0,0 +1,24 @@
+const social = [
+    {
+        vector: '/images/github-thumbnail.svg',
+        label: 'GitHub',
+        url: 'https://github.com/mojocodeio'
+    },
+    {
+        vector: '/images/linkedin-thumbnail.svg',
+        label: 'LinkedIn',
+        url: 'https://www.linkedin.com/in/joey-schrader-472388aa/'
+    },
+    {
+        vector: '/images/twitter-thumbnail.svg',
+        label: 'Twitter',
+        url: 'https://twitter.com/joey_mojocode'
+    },
+    {
+        vector: '/images/gmail-thumbnail.svg',
+        label: 'Gmail',
+        url: 'mailto:jschrader@mojocode.io'
+    },
+]
+
+module.exports = social
